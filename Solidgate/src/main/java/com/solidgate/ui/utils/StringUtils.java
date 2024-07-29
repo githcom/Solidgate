c71956070ea2e7a274e9f1118c131f5a179c3a3f
githcom <githcom@users.noreply.github.com>
2024-07-30 00:32:21 +0200

    Delete Solidgate directory

--- Solidgate/src/main/java/com/solidgate/ui/utils/StringUtils.java
+++
@@ -1,8 +0,0 @@
-package com.solidgate.ui.utils;
-
-public class StringUtils {
-
-    public static int removeDotsAndParseToInt(String value) {
-        return Integer.parseInt(value.replaceAll("[.]", ""));
-    }
-}
